c32d774dfb66f7babdda76bb81c5b4293e2c250e
roocell <roocell@5f95faa4-7374-46de-a021-085feb9a3d16>
2014-04-19 15:28:03 +0000

    maps elevation

---
+++ MapsElevationTests/MapsElevationTests.m
@@ -0,0 +1,34 @@
+//
+//  MapsElevationTests.m
+//  MapsElevationTests
+//
+//  Created by michael russell on 2014-04-05.
+//  Copyright (c) 2014 ThumbGenius Software. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MapsElevationTests : XCTestCase
+
+@end
+
+@implementation MapsElevationTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
