09c16f9a6511cccefb6b791d0311b865ec2112f5
roocell <roocell@5f95faa4-7374-46de-a021-085feb9a3d16>
2014-04-19 15:20:04 +0000

    importing maps elevation

---
+++ MapsElevation/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MapsElevation
+//
+//  Created by michael russell on 2014-04-05.
+//  Copyright (c) 2014 ThumbGenius Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "elevationAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([elevationAppDelegate class]));
+    }
+}
