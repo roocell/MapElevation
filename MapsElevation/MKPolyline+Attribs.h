c410eb096b5516790db7a831ee90d6f72b4224f5
roocell <roocell@5f95faa4-7374-46de-a021-085feb9a3d16>
2014-06-16 23:45:08 +0000

    before spotfinder integration

---
+++ MapsElevation/MKPolyline+Attribs.h
@@ -0,0 +1,14 @@
+//
+//  MKPolyline+Attribs.h
+//  MapsElevation
+//
+//  Created by michael russell on 2014-05-22.
+//  Copyright (c) 2014 ThumbGenius Software. All rights reserved.
+//
+
+#import <MapKit/MapKit.h>
+
+@interface MKPolyline (Attribs)
+@property (retain, nonatomic) UIColor* color;
+
+@end
